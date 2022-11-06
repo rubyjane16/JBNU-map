d2e279fb681ef9039ddd40e084ec1e226ff84216
rubyjane16 <89911621+rubyjane16@users.noreply.github.com>
2022-11-06 17:23:06 +0900

    Update DatabaseUtil.java

--- java/util/DatabaseUtil.java
+++ java/util/DatabaseUtil.java
@@ -6,9 +6,12 @@ import java.sql.DriverManager;
 public class DatabaseUtil {
 	public static Connection getConnection() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/jbnumap";
-			String dbID = "root";
-			String dbPassword = "201946725";
+			String dbURL = "jdbc:mysql://52.79.233.229:8080/jbnu/";
+
+      			String dbID = "root";
+
+        		String dbPassword = "****";
+
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
